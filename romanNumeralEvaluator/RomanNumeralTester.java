654eff774ee40d8cad9d6e494bcf41ae4a48fbf7
TheGreatKetchup <mattschweiss31415@gmail.com>
2016-12-29 09:47:23 -0500

    Added Test Main
    Added test code for this function. Works ok.

---
+++ romanNumeralEvaluator/RomanNumeralTester.java
@@ -0,0 +1,16 @@
+package romanNumeralEvaluator;
+
+public class RomanNumeralTester {
+	
+	private static RomanNumeralEvaluator numeralConverter = new RomanNumeralEvaluator();
+
+	public static void main(String[] args) {
+		//Generate the Roman Numerals.
+		System.out.println("Hello. Welcome to the Roman Numeral Tester");
+		System.out.print("66 in Roman Numerals is ");
+		System.out.println(numeralConverter.convertNumberToRomanNumeral(66));
+		System.out.print("XXII in decimal is ");
+		System.out.println(numeralConverter.convertRomanNumeralToNumber("XXII"));
+	}
+
+}
